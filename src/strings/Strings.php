aece113075a588c5886c9e800fe0456789921611
Artem Nikulin <artr.nik20@gmail.com>
2022-08-16 13:41:17 +0300

    Make fixes in Strings class

--- src/strings/Strings.php
+++ src/strings/Strings.php
@@ -1,38 +1,35 @@
 <?php
 
-
 namespace strings;
-
 
 class Strings implements StringsInterface
 {
-
     public function snakeCaseToCamelCase(string $input): string
     {
-        $firstStr = mb_convert_case(str_replace("_", " ", $input), MB_CASE_TITLE);
-        return lcfirst(str_replace(" ", "", $firstStr));
+        return lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $input))));
     }
 
     public function mirrorMultibyteString(string $input): string
     {
         $wordsArr = explode(" ", $input);
         $new_string = [];
-        foreach ($wordsArr as $word){
-            $new_string[] = iconv('utf-16be', 'utf-8', strrev(
-                iconv('utf-8', 'utf-16le', $word)
-            ));
+
+        foreach ($wordsArr as $word) {
+            $new_string[] = iconv('utf-16be', 'utf-8', strrev(iconv('utf-8', 'utf-16le', $word)));
         }
+
         return implode(" ", $new_string);
     }
 
     public function getBrandName(string $noun): string
     {
-        $firstLetter = $noun[0];
-        $lastLetter = $noun[-1];
-        if($firstLetter === $lastLetter) {
-            return ucfirst($noun) . substr($noun, 1);
-        }else{
-            return "The " . ucfirst($noun);
+        if ($noun[0] === $noun[-1]) {
+            $brandName = ucfirst($noun) . substr($noun, 1);
+        } else {
+            $brandName = "The " . ucfirst($noun);
         }
+        return $brandName;
+
+//        return ($noun[0] === $noun[-1]) ? (ucfirst($noun) . substr($noun, 1)) : ("The " . ucfirst($noun));
     }
 }
