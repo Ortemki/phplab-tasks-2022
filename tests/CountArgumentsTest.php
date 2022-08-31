d907d6a906a9152838166992c3bb698dec1f6c91
Artem Nikulin <artr.nik20@gmail.com>
2022-08-31 09:47:33 +0300

    Change CountArgumentsTest

--- tests/CountArgumentsTest.php
+++ tests/CountArgumentsTest.php
@@ -12,48 +12,19 @@ class CountArgumentsTest extends TestCase
     }
 
     /**
-     * @dataProvider noArgumentsProvider
+     * @dataProvider positiveDataProvider
      */
-    public function testNoArguments($input, $expected)
+    public function testPositive(array $input, array $expected)
     {
         $this->assertEquals($expected, $this->functions->countArguments($input));
     }
 
-    public function noArgumentsProvider(): array
-    {
-        return[
-            [[],['argument_count' => 1, 'argument_values' => array(array())]]
-        ];
-    }
-
-    /**
-     * @dataProvider oneStringProvider
-     */
-    public function testOneStringPositive($input, $expected)
-    {
-        $this->assertEquals($expected, $this->functions->countArguments($input));
-    }
-
-    public function oneStringProvider(): array
+    public function positiveDataProvider(): array
     {
         return [
-            ['a', ['argument_count' => 1, 'argument_values' => array('a')]]
-        ];
-    }
-
-    /**
-     * @dataProvider coupleStringsProvider
-     */
-
-    public function testCoupleStringsPositive($input1, $input2, $expected)
-    {
-        $this->assertEquals($expected, $this->functions->countArguments($input1, $input2));
-    }
-
-    public function coupleStringsProvider(): array
-    {
-        return [
-            ['a', 'b', ['argument_count' => 2, 'argument_values' => array('a','b')]]
+            [[],['argument_count' => 1, 'argument_values' => array(array())]],
+            [['a'], ['argument_count' => 1, 'argument_values' => array(array('a'))]],
+            [['a', 'b'], ['argument_count' => 1, 'argument_values' => array(array('a','b'))]]
         ];
     }
 }
